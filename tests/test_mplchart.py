1b7c44bc2958c1f8180e718121683aa8f988c8f7
Furechan <furechan@xsmail.com>
2023-10-30 04:48:26 -0400

    ...

---
+++ tests/test_mplchart.py
@@ -0,0 +1,26 @@
+import pytest
+
+from mplchart import samples
+from mplchart.chart import Chart
+from mplchart.primitives import Candlesticks, Volume
+from mplchart.indicators import SMA, EMA, RSI, MACD, PPO, BBANDS
+
+INDICATORS = [Volume(), SMA(50), EMA(50), RSI(), MACD(), PPO(), BBANDS()]
+
+
+@pytest.fixture
+def prices():
+    return samples.sample_prices()
+
+
+@pytest.mark.parametrize("indicator", INDICATORS, ids=str)
+def test_chart(prices, indicator, max_bars=250):
+    if indicator is None:
+        indicator = SMA(50)
+
+    indicators = [Candlesticks(), indicator]
+
+    chart = Chart(title="Test", max_bars=max_bars)
+    chart.plot(prices, indicators)
+
+    assert chart.count_axes() > 0
