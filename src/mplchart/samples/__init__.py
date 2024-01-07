1520602bf776455764af470c71b2a0a9c3e62d05
Furechan <mancuso@fastmail.com>
2024-01-07 17:41:04 -0500

    Using importlib.resources in samples

--- src/mplchart/samples/__init__.py
+++ src/mplchart/samples/__init__.py
@@ -4,10 +4,20 @@ import pandas as pd
 
 from pathlib import Path
 
+from importlib.resources import open_text, is_resource
 
-def sample_prices():
+
+def sample_prices_old():
     """Sample prices"""
 
     folder = Path(__file__).parent
     file = folder.joinpath("sample-prices.csv")
     return pd.read_csv(file, index_col=0, parse_dates=True)
+
+
+def sample_prices():
+    """Sample prices"""
+
+    file = open_text(__name__, "sample-prices.csv")
+    return pd.read_csv(file, index_col=0, parse_dates=True)
+
