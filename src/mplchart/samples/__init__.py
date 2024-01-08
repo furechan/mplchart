ee64fd10d1bd4c2517f3f9daf8f2f7b6e8f14683
Furechan <mancuso@fastmail.com>
2024-01-07 22:54:43 -0500

    Update Python version and license information

--- src/mplchart/samples/__init__.py
+++ src/mplchart/samples/__init__.py
@@ -2,11 +2,12 @@
 
 import pandas as pd
 
-from importlib.resources import open_text
+from importlib import resources
 
 
 def sample_prices():
     """Sample prices"""
 
-    file = open_text(__name__, "sample-prices.csv")
-    return pd.read_csv(file, index_col=0, parse_dates=True)
+    res = resources.files(__name__).joinpath("sample-prices.csv")
+    with resources.as_file(res) as file:
+        return pd.read_csv(file, index_col=0, parse_dates=True)
