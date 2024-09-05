f6f92d43433ad39d4a5618d4ebc2d8e78fa51f0d
Furechan <mancuso@fastmail.com>
2024-09-05 17:58:39 +0800

    Added streamlit example

---
+++ examples/mplchart-streamlit.py
@@ -0,0 +1,27 @@
+""" mplchart with streamlit example """
+
+import streamlit as st
+import yfinance as yf
+
+from mplchart.chart import Chart
+from mplchart.primitives import Candlesticks, Volume
+from mplchart.indicators import ROC, SMA, EMA, RSI, MACD
+
+ticker = 'AAPL'
+prices = yf.Ticker(ticker).history('5y')
+
+st.dataframe(prices.tail())
+
+max_bars = 250
+
+indicators = [
+    Candlesticks(), SMA(50), SMA(200), Volume(),
+    RSI(),
+    MACD(),
+]
+
+chart = Chart(title=ticker, max_bars=max_bars)
+chart.plot(prices, indicators)
+
+st.pyplot(chart.figure)
+
